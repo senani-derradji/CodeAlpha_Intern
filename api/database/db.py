ccde78b0fd2bfdf21cf764fa187c828d1110f082
senani-derradji <derradji.senani.1@gmail.com>
2026-05-21 22:14:28 +0000

    check the import for Base

--- api/database/db.py
+++ api/database/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from api.config.settings import Info
 from database.models import User, ClickersInfo, ShortUrl
-
+from api.database.base import Base
 
 DATABASE_URL = Info.SQLALCHEMY_DATABASE_URL
 
@@ -17,8 +17,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-Base = declarative_base()
 
 def init_db():
     Base.metadata.create_all(bind=engine)
